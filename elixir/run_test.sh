b0469aa31b5b13d2e85248e84cd449046bd89d26
ryosan470 <rskjtwp@gmail.com>
2020-04-21 21:21:01 +0900

    fix ci script

--- elixir/run_test.sh
+++ elixir/run_test.sh
@@ -4,14 +4,17 @@ function run_test() {
     TARGET_PATH=$1
 
     if [ -f ${TARGET_PATH}/mix.exs ]; then
+        cd $TARGET_PATH
+        mix deps.get
         mix test
+        cd ../
     else
         elixir ${TARGET_PATH}/*_test.exs
     fi
 }
 
 function run_tests() {
-    directories=$(find "." -type d -not -path ".")
+    directories=$(find "." -type d -not -path "." -maxdepth 1)
     for d in ${directories}; do
         run_test $d;
     done
@@ -19,5 +22,5 @@ function run_tests() {
 
 case $1 in
     "all") run_tests ;;
-    "*") run_test $1 ;;
+    *) run_test $1 ;;
 esac
