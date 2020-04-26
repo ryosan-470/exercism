d5689b0c6e44b082173c1b01828cd296b4d5f2a0
ryosan-470 <rskjtwp@gmail.com>
2020-04-26 17:10:59 +0900

    [elixir] Solve "Leap"

---
+++ elixir/leap/lib/year.ex
@@ -0,0 +1,20 @@
+defmodule Year do
+  @doc """
+  Returns whether 'year' is a leap year.
+
+  A leap year occurs:
+
+  on every year that is evenly divisible by 4
+    except every year that is evenly divisible by 100
+      unless the year is also evenly divisible by 400
+  """
+  @spec leap_year?(non_neg_integer) :: boolean
+  def leap_year?(year) do
+    case {rem(year, 4) == 0, rem(year, 100) == 0, rem(year, 400) == 0} do
+      {false, _, _} -> false
+      {true, true, true} -> true
+      {true, true, false} -> false
+      _ -> true
+    end
+  end
+end
